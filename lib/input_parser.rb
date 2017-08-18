3b8c4125908aa5bb2dced144c7a29a9331f2b514
Takuya Mukohira <takuya.mk96@gmail.com>
2017-08-18 15:14:00 +0900

    Add InputParser

--- lib/input_parser.rb
+++ lib/input_parser.rb
@@ -1,3 +1,5 @@
 class InputParser
-
+  def execute(input)
+    input.split.map(&:to_f)
+  end
 end
--- spec/lib/input_parser_spec.rb
+++ spec/lib/input_parser_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 require 'input_parser'
 
 describe InputParser do
+  before { $stdin = StringIO.new("0\n10\n250\n100\n1.5") }
+
   describe '#initialize' do
     let(:parser) { InputParser.new }
     subject { parser }
 
     it { is_expected.to be_kind_of InputParser }
+  end
+
+  describe '#execute' do
+    let(:parser) { InputParser.new }
+
+    it '標準入力を受け取って改行区切りにして配列にして返す' do
+      expect(parser.execute($stdin.read)).to match_array [0.0, 10.0, 250.0, 100.0, 1.5]
+    end
   end
 end
