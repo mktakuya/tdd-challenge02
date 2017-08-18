0c8b0db7c19e7196a899da029404dabf6b26e8e3
Takuya Mukohira <takuya.mk96@gmail.com>
2017-08-18 14:28:53 +0900

    Create Circle class

--- spec/lib/circle_spec.rb
+++ spec/lib/circle_spec.rb
@@ -1,3 +1,15 @@
+require 'spec_helper'
+require 'circle'
+
 describe Circle do
-  it 'works'
+  describe '#initialize' do
+    let(:circle) { Circle.new(5) }
+    subject { circle }
+
+    it { is_expected.to be_kind_of Circle }
+
+    it 'インスタンス変数@radiusに引数がセットされること' do
+      expect(circle.instance_variable_get(:@radius)).to eq 5
+    end
+  end
 end
